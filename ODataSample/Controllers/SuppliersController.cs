688cf3b87ca04f41d674897458fdcee42d4bf905
arch-imp <kentho_98@yahoo.com>
2018-05-20 15:42:15 -0600

    Getting a related collection

--- ODataSample/Controllers/SuppliersController.cs
+++ ODataSample/Controllers/SuppliersController.cs
@@ -119,5 +119,12 @@ namespace ODataSample.Controllers
             await db.SaveChangesAsync();
             return StatusCode(HttpStatusCode.NoContent);
         }
+
+        // GET /Suppliers(1)/Products
+        [EnableQuery]
+        public IQueryable<Product> GetProducts([FromODataUri] int key)
+        {
+            return db.Suppliers.Where(m => m.Id.Equals(key)).SelectMany(m => m.Products);
+        }
     }
 }
