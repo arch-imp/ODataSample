00f97a2a82c1cce8eee18790da5663b4f7dc81b9
arch-imp <kentho_98@yahoo.com>
2018-05-20 13:53:54 -0600

    Updating an Entity

--- ODataSample/Controllers/ProductsController.cs
+++ ODataSample/Controllers/ProductsController.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.OData;
@@ -44,6 +46,65 @@ namespace ODataSample.Controllers
             db.Products.Add(product);
             await db.SaveChangesAsync();
             return Created(product);
+        }
+
+        public async Task<IHttpActionResult> Patch([FromODataUri] int key, Delta<Product> product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var entity = await db.Products.FindAsync(key);
+            if (entity == null)
+            {
+                return NotFound();
+            }
+            product.Patch(entity);
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return Updated(entity);
+        }
+
+        public async Task<IHttpActionResult> Put([FromODataUri] int key, Product update)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (key != update.Id)
+            {
+                return BadRequest();
+            }
+            db.Entry(update).State = EntityState.Modified;
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return Updated(update);
         }
     }
 }
