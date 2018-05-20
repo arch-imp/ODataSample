e94998d8e484f49250a6d45a456abcbe944ad9b5
arch-imp <kentho_98@yahoo.com>
2018-05-20 13:36:18 -0600

    Add ProductsController.

---
+++ ODataSample/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Web.OData;
+using ODataSample.Models;
+
+namespace ODataSample.Controllers
+{
+    public class ProductsController : ODataController
+    {
+        ProductsContext db = new ProductsContext();
+
+        private bool ProductExists(int key)
+        {
+            return db.Products.Any(p => p.Id == key);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
