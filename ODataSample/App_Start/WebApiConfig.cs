8a1aacc8e07b20cedf0d1122831e76cba851e7b0
arch-imp <kentho_98@yahoo.com>
2018-05-20 15:29:56 -0600

    Add a "Suppliers" entity set to the entity data model.

--- ODataSample/App_Start/WebApiConfig.cs
+++ ODataSample/App_Start/WebApiConfig.cs
@@ -12,6 +12,7 @@ namespace ODataSample
             // New code:
             ODataModelBuilder builder = new ODataConventionModelBuilder();
             builder.EntitySet<Product>("Products");
+            builder.EntitySet<Supplier>("Suppliers");
             config.MapODataServiceRoute("ODataRoute", null, builder.GetEdmModel());
         }
     }
