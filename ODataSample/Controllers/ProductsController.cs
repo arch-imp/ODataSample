91c5d35c651eb0e7b0c83b27548f619e61de7088
arch-imp <kentho_98@yahoo.com>
2018-05-20 16:26:47 -0600

    Deleting a Relationship Between Entities

--- ODataSample/Controllers/ProductsController.cs
+++ ODataSample/Controllers/ProductsController.cs
@@ -159,5 +159,27 @@ namespace ODataSample.Controllers
             await db.SaveChangesAsync();
             return StatusCode(HttpStatusCode.NoContent);
         }
+
+        public async Task<IHttpActionResult> DeleteRef([FromODataUri] int key, string navigationProperty, [FromBody] Uri link)
+        {
+            var product = db.Products.SingleOrDefault(p => p.Id == key);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            switch (navigationProperty)
+            {
+                case "Supplier":
+                    product.Supplier = null;
+                    break;
+
+                default:
+                    return StatusCode(HttpStatusCode.NotImplemented);
+            }
+            await db.SaveChangesAsync();
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
     }
 }
