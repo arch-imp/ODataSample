ad901392826fcb1fb8b0938fb8d0c2bf38d561fb
arch-imp <kentho_98@yahoo.com>
2018-05-20 15:25:45 -0600

    Add a Supplier Entity

---
+++ ODataSample/Models/Supplier.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using ODataSample.Models;
+
+namespace ProductService.Models
+{
+    public class Supplier
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
